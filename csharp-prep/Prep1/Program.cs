4962bea60dfac3de888cfa5681abf0b92a327976
akani28 <nietocevallos61@gmail.com>
2024-03-03 03:22:36 -0500

    update prep#1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? ");
+        String firstName = Console.ReadLine();
+        firstName = firstName.ToUpper().Substring(0 ,1) + firstName.Substring(1);
+        Console.Write("What is your last name? ");
+        String lastName = Console.ReadLine();
+        lastName = lastName.ToUpper().Substring(0,1)+lastName.Substring(1);
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
